c603c6704e88187018ab66ddf120f7c7f2975f8e
月 <yue02469@gmail.com>
2026-02-10 02:49:25 +0900

    .

--- powernote/svelte.config.js
+++ powernote/svelte.config.js
@@ -3,18 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(), // これを忘れるとSvelteファイル内のCSSでエラーが出ることがあります
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html', // 追記：GitHub Pagesでリロードした際のエラーを防ぐ
+			fallback: '404.html',
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-			// 追記：リポジトリ名が "PowerNote" なら '/PowerNote' に書き換えてください
-			// カスタムドメインを使う場合は空文字 '' でOKです
+			// ここをリポジトリ名に合わせて修正
 			base: process.env.NODE_ENV === 'production' ? '/PowerNote' : '',
 		}
 	}
